cb5451cfabf274599433a63b865f77f79e55d0dd
Zoltán Domahidi <domahidizoltan@gmail.com>
2023-12-18 15:31:07 +0100

    3-limit-service-time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,13 @@
 
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+const maxFreeProcessingTimeSeconds = 10
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,7 +28,38 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	ticker := time.NewTicker(time.Second)
+	doneCh := make(chan struct{})
+	defer func() {
+		// close(doneCh)
+		ticker.Stop()
+		// recover()
+	}()
+
+	start := time.Now()
+
+	go func() {
+		for range ticker.C {
+			u.TimeUsed += 1
+			if !u.IsPremium && u.TimeUsed >= maxFreeProcessingTimeSeconds-1 {
+				fmt.Println("Free processing time is over for UserID:", u.ID)
+
+				doneCh <- struct{}{}
+				return
+			}
+		}
+	}()
+
+	go func() {
+		process()
+		doneCh <- struct{}{}
+	}()
+
+	<-doneCh
+
+	timeSpentSeconds := int64(time.Since(start).Seconds())
+	fmt.Printf("Processed %ds (total %ds) for UserID: %d\n", timeSpentSeconds, u.TimeUsed, u.ID)
+
 	return true
 }
 
